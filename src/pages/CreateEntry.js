51039e2da53d640419f209f654ef63c72a2a6976
alhsu713 <allison.hsu20@gmail.com>
2022-11-27 20:51:33 -0500

    add create entry page

--- src/pages/CreateEntry.js
+++ src/pages/CreateEntry.js
@@ -1,27 +1,41 @@
 import React from "react";
 
 const CreateEntry = () => (
-  <div>
-    <h1 className="title is-1">Create Food Entry</h1>
+  <div className="Content">
+
     <form>
+      <h1 >Create Food Entry</h1>
       <label>
         Food Name:
         <input type="text" name="Food Name" />
       </label>
       <label>
-        Allergens:
-        <input type="text" name="Allergens" />
+        Dietary Restrictions:
+        <div class="checkbox-grid">
+          <ul >
+            <li><input type="checkbox" name="wheat" value="value1" /><label className="allergy" for="wheat">Wheat</label></li>
+            <li><input type="checkbox" name="shellfish" value="value2" /><label className="allergy" for="shellfish">Shellfish</label></li>
+            <li><input type="checkbox" name="eggs" value="value3" /><label className="allergy" for="eggs">Eggs</label></li>
+            <li><input type="checkbox" name="treenuts" value="value4" /><label className="allergy" for="treenuts">Tree Nuts</label></li>
+            <li><input type="checkbox" name="soy" value="value5" /><label className="allergy" for="soy">Soy</label></li>
+            <li><input type="checkbox" name="dairy" value="value6" /><label className="allergy" for="dairy">Dairy</label></li>
+            <li><input type="checkbox" name="peanuts" value="value7" /><label className="allergy" for="peanuts">Peanuts</label></li>
+            <li><input type="checkbox" name="vegetarian" value="value8" /><label className="allergy" for="vegetarian">Vegetarian</label></li>
+            <li><input type="checkbox" name="other" value="value8" /><label className="allergy" for="other">Other</label></li>
+          </ul>
+          <div class="clr"></div>
+        </div>
       </label>
       <label>
         Number of Servings/Packages:
         <input type="text" name="Size" />
       </label>
       <label>
-        Loacaion:
+        Location:
         <input type="text" name="Location" />
       </label>
       <label>
-        Dropoff Time:
+        Pick up food by:
         <input type="time" name="Dropoff" />
       </label>
       <label>
@@ -32,7 +46,7 @@ const CreateEntry = () => (
         Image Description:
         <input type="text" name="Description" />
       </label>
-      <input type="submit" value="Submit" />
+      <a className="ClaimButton" href="\entry">SUBMIT</a>
     </form>
 
   </div>
