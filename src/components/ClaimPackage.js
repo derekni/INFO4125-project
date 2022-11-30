673dd77e22a75f88c5a42dee0688fcf4b879de01
derekni <dan82@cornell.com>
2022-11-29 20:51:07 -0500

    update styling for entry

--- src/components/ClaimPackage.js
+++ src/components/ClaimPackage.js
@@ -6,46 +6,62 @@ const ClaimPackage = (props) => {
   return props.trigger ? (
     <div className="ClaimPackage">
       <div className="ClaimPackage-inner">
-        {confirm ? (
-          <>
+        {!confirm ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "100%",
+            }}
+          >
             <h2>Claim Package</h2>
             <h4>1x Chipotle Bowl</h4>
             <h4>Location: Gates Hall Room G01</h4>
             <h4>Available until: 6:30pm</h4>
-            <div style={{ display: "flex", flexDirection: "row" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
               <button
                 onClick={() => {
                   props.cancel();
                 }}
+                className="ReturnButton"
               >
-                RETURN TO FEED
+                RETURN TO POSTING
               </button>
               <button
                 onClick={() => {
                   setConfirm(true);
                 }}
+                className="ClaimButton"
               >
                 CLAIM PACKAGE
               </button>
             </div>
-          </>
+          </div>
         ) : (
-          <>
-            <div>Claim Package</div>
-            <div>1x Chipotle Bowl</div>
-            <div>Location: Gates Hall Room G01</div>
-            <div>Available until: 6:30pm</div>
-            <div>
-              <button>RETURN TO FEED</button>
-              <button
-                onClick={() => {
-                  setConfirm(true);
-                }}
-              >
-                CLAIM PACKAGE
-              </button>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "100%",
+            }}
+          >
+            <h3>You successfully requested Chipotle Bowl from Mariana Lowe</h3>
+            <h4>Location: Gates Hall Room G01</h4>
+            <h4>Available until: 6:30pm</h4>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <a href="/" className="ReturnButton">
+                RETURN TO FEED
+              </a>
             </div>
-          </>
+          </div>
         )}
       </div>
     </div>
